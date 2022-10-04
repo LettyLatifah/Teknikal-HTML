b5daa7fbdae0dbbc958c2aea7ed31d08ef2d42f8
LettyLatifah <lettylatifah15@gmail.com>
2022-10-04 15:04:23 +0700

    javascript

--- praktik/script2.js
+++ praktik/script2.js
@@ -1,9 +1,27 @@
-let umur = 10;
+let umur = 12;
 
 if (umur <= 17) {
   console.log('Masih Kecil');
 } else if (umur > 17 && umur <= 30) {
   console.log('Masih Remaja');
-} else if (umur > 30) {
+} else if (umur > 30 && umur < 70) {
   console.log('sudah tua');
+} else {
+  console.log('Sudah Meninggal');
 }
+
+let a = 15;
+let b = 10;
+
+console.log(a < 20 && b > 5); 
+// dua kondisi harus terpenuhi jika ingin mendapatkan nilai true
+console.log(a < 30 || b < 5);
+// cukup memenuhi satu kondisi untuk mendapatlan nilai true
+
+let c = 10;
+let d = "10";
+
+console.log(c == d);
+// hanya membadingkna isi nilainya
+console.log (c === d);
+// membandingkan isi nilai dan tipe data
