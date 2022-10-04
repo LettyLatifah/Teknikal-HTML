fe67f2455c5a01a91160ea1f65c8d017f844c5c0
LettyLatifah <lettylatifah15@gmail.com>
2022-10-04 14:53:17 +0700

    Javascript Material

---
+++ praktik/script.js
@@ -0,0 +1,36 @@
+const x = 'Belajar Javascript';
+let k = 23;
+var b = true;
+
+console.log(x);
+console.log(k);
+console.log(b);
+
+
+// var c = "halo";
+
+// console.log(c);
+
+let a = 23;
+
+let string = 'belajar javascript';
+let object = {
+  nama: 'Letty',
+  umur: 21,
+  kerja: false,
+};
+let array = [1, 2, 3, 4, 5];
+
+let boolean = true;
+
+console.log(typeof a);
+console.log(string);
+console.log(object);
+console.log(array);
+console.log(array[2]);
+console.log(boolean);
+
+
+// variabel yang sering digunakan adalah let dan const
+// let cakupannya global, dan variabelnya tidak bisa didefiniskan pada file lain
+// const isi dari variabelnya tidak bisa diubah lagi
---
+++ praktik/script2.js
@@ -0,0 +1,9 @@
+let umur = 10;
+
+if (umur <= 17) {
+  console.log('Masih Kecil');
+} else if (umur > 17 && umur <= 30) {
+  console.log('Masih Remaja');
+} else if (umur > 30) {
+  console.log('sudah tua');
+}
